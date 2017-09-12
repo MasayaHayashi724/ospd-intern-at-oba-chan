79f09dd66d0729edc167064867e18358c8b85bf5
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-09-12 16:59:47 +0900

    Create TimelineViewController

---
+++ ospd-intern-at-oba-chan/TimelineViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TimelineViewController.swift
+//  ospd-intern-at-oba-chan
+//
+//  Created by Masaya Hayashi on 2017/09/12.
+//  Copyright © 2017年 Masaya Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
