c3840fa3190057988aee4e1a4016b1720374a8ff
Kansei Kogami <k_kansei_k@yahoo.co.jp>
2017-09-13 11:10:54 +0900

    add camera

--- ospd-intern-at-oba-chan/TimelineViewController.swift
+++ ospd-intern-at-oba-chan/TimelineViewController.swift
@@ -10,64 +10,64 @@ import UIKit
 import Social
 import Accounts
 
-class TimelineViewController: UIViewController {
-
+class TimelineViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
     @IBOutlet weak var timelineTableView: UITableView!
-
+    
     var accountStore = ACAccountStore()
     var twitterAccount: ACAccount?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         selectTwitterAccount()
     }
-
+    
     private func selectTwitterAccount() {
         let accountType = accountStore.accountType(withAccountTypeIdentifier: ACAccountTypeIdentifierTwitter)
-
+        
         accountStore.requestAccessToAccounts(with: accountType, options: nil) { (granted, error) in
             if let error = error {
                 print(error)
                 return
             }
-
+            
             guard granted else {
                 print("error! Twitterアカウントの利用が許可されていません")
                 return
             }
-
+            
             let accounts = self.accountStore.accounts(with: accountType) as! [ACAccount]
             if accounts.isEmpty {
                 print("error! 設定画面からアカウントを設定してください")
                 return
             }
-
+            
             self.showAccountSelectSheet(accounts: accounts)
         }
     }
-
+    
     private func showAccountSelectSheet(accounts: [ACAccount]) {
         let alert = UIAlertController(title: "Twitter", message: "アカウントを選択してください", preferredStyle: .actionSheet)
-
+        
         for account in accounts {
             alert.addAction(UIAlertAction(title: account.username, style: .default, handler: { _ in
                 self.twitterAccount = account
                 self.getTimeline()
             }))
         }
-
+        
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-
+        
         self.present(alert, animated: true, completion: nil)
     }
-
+    
     private func getTimeline() {
         let url = URL(string: "https://api.twitter.com/1.1/statuses/user_timeline.json")!
-
+        
         guard let request = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: .GET, url: url, parameters: nil) else { return }
-
+        
         request.account = twitterAccount
-
+        
         request.perform { (data, response, error) in
             if let error = error {
                 print(error)
@@ -82,19 +82,53 @@ class TimelineViewController: UIViewController {
             }
         }
     }
-
+    
     @IBAction func takePhotoButtonTapped(_ sender: Any) {
-        postTweet()
+        //カメラを起動する
+        let sourceType:UIImagePickerControllerSourceType = UIImagePickerControllerSourceType.camera
+        // カメラが利用可能かチェック
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
+            // インスタンスの作成
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            self.present(cameraPicker, animated: true, completion: nil)
+            
+        }
+        else{
+            
+        }
     }
-
-    private func postTweet() {
+    
+        //撮影が完了時した時に呼ばれる
+        func imagePickerController(_ imagePicker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    
+           if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+//                cameraView.contentMode = .scaleAspectFit
+//                cameraView.image = pickedImage
+                postTweet(image: pickedImage)
+    
+            }
+    
+            //閉じる処理
+            imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    // 撮影がキャンセルされた時に呼ばれる
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+            private func postTweet(image: UIImage) {
         let url = URL(string: "https://api.twitter.com/1.1/statuses/update.json")!
         let params = ["status": "Yeah"]
-
+        
         guard let request = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: .POST, url: url, parameters: params) else { return }
-
+        
         request.account = twitterAccount
-
+        
         request.perform { (data, response, error) in
             if let error = error {
                 print(error)
@@ -108,17 +142,23 @@ class TimelineViewController: UIViewController {
             }
         }
     }
-
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
 }
 
 extension TimelineViewController: UITableViewDataSource {
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
     }
-
+    
 }
--- ospd-intern-at-oba-chan/ViewController.swift
+++ ospd-intern-at-oba-chan/ViewController.swift
@@ -17,43 +17,49 @@ class ViewController: UIViewController {
     @IBOutlet weak var groupName: UITextField!
     
     @IBAction func coopTwitter(_ sender: UIButton) {
-        if myID.text != "" && makeID.text != "" && phoneNum.text != "" && groupName.text != ""{
-            let myID = self.myID.text!
-            let makeID  = self.makeID.text!
-            let phoneNum = self.phoneNum.text!
-            let groupName = self.groupName.text!
-            
-            // 接続先のURLにパラメータを追加
-            let stringUrl = "http://localhost:3000/test/?myID=\(myID)&makeID=\(makeID)&phoneNum=\(phoneNum)&groupName=\(groupName)"
-            
-            // 文字列のURLからNSURLに変換
-            let URL = NSURL(string:stringUrl)!
-            // NSURLを引数にRequestの生成
-            //let req = URLRequest(url: URL! as URL)
-            
-            // ここからが実際に通信をする所（非同期通信）
-//            let task = URLSession.shared.dataTask(with: req, completionHandler: {
-//                (data, res, err) in
-            
-            let task = URLSession.shared.dataTask(with: URL as URL, completionHandler: {data, response, error in
-//                // サーバから出力はありますか？
-             if data != nil {
-//                    // あるみたいだから文字列に変換しておく
-                    let text = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
-                print(text!)
-//                    // 非同期通信内でUIに変更を加えるときのおまじないと思って！
-//                    DispatchQueue.main.async(execute: {
-//                        self.resultLabel.text = text as String?
-//                    })
-//                }else{
-//                    // サーバから返事がないみたいだからエラー表示
-//                    DispatchQueue.main.asyncdispatch_async(DispatchQueue.main,execute: {
-//                        self.resultLabel.text = "ERROR"
-//                    })
-                }
-            })
-            task.resume()
-        }
+        
+        
+//ここから
+//        if myID.text != "" && makeID.text != "" && phoneNum.text != "" && groupName.text != ""{
+//            let myID = self.myID.text!
+//            let makeID  = self.makeID.text!
+//            let phoneNum = self.phoneNum.text!
+//            let groupName = self.groupName.text!
+//            
+//            // 接続先のURLにパラメータを追加
+//            let stringUrl = "http://localhost:3000/test/?myID=\(myID)&makeID=\(makeID)&phoneNum=\(phoneNum)&groupName=\(groupName)"
+//            
+//            // 文字列のURLからNSURLに変換
+//            let URL = NSURL(string:stringUrl)!
+//            // NSURLを引数にRequestの生成
+//            //let req = URLRequest(url: URL! as URL)
+//            
+//            // ここからが実際に通信をする所（非同期通信）
+////            let task = URLSession.shared.dataTask(with: req, completionHandler: {
+////                (data, res, err) in
+//            
+//            let task = URLSession.shared.dataTask(with: URL as URL, completionHandler: {data, response, error in
+////                // サーバから出力はありますか？
+//             if data != nil {
+////                    // あるみたいだから文字列に変換しておく
+//                    let text = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+//                print(text!)
+////                    // 非同期通信内でUIに変更を加えるときのおまじないと思って！
+////                    DispatchQueue.main.async(execute: {
+////                        self.resultLabel.text = text as String?
+////                    })
+////                }else{
+////                    // サーバから返事がないみたいだからエラー表示
+////                    DispatchQueue.main.asyncdispatch_async(DispatchQueue.main,execute: {
+////                        self.resultLabel.text = "ERROR"
+////                    })
+//                }
+//            })
+//            task.resume()
+//        }
+//ここまで
+        
+        
     }
     
     override func viewDidLoad() {
